7c4931717577e57a26887abc013759dd4f1f8e11
buster42b <61701447+buster42b@users.noreply.github.com>
2020-12-20 22:54:07 +0300

    Add files via upload

---
+++ lab1/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+exec 0<>/dev/console 1<>/dev/console 2<>/dev/console 
+cat <<'msgend'
+
+                                   poker
+
+                                   .   .
+
+                                  _______
+
+                                   face
+msgend
+sleep 100
+echo " continuing.... " 
